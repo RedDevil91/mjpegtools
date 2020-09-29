93df31c48834ec537bd6291a12ee8cf90f22ddbb
Attila Bognar <attila.bognar@logmein.com>
2020-09-29 19:56:40 +0200

    init base64 support

--- lavtools/jpeg2yuv.c
+++ lavtools/jpeg2yuv.c
@@ -26,6 +26,7 @@ jpeg2yuv
 #include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 
 #include <string.h>
@@ -73,6 +74,7 @@ typedef struct _parameters {
   int rescale_YUV;
 } parameters_t;
 
+
 typedef struct _read_parameters {
     ssize_t buff_size;
     uint8_t *frame_buff;
@@ -81,6 +83,18 @@ typedef struct _read_parameters {
 
 static struct jpeg_decompress_struct dinfo;
 static struct jpeg_error_mgr jerr;
+
+
+static char encoding_table[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/' };
+static char* decoding_table = NULL;
+static int mod_table[] = { 0, 2, 1 };
 
 
 /*
@@ -464,6 +478,55 @@ static ssize_t read_jpeg_from_stdin(uint8_t* jpegdata, read_parameters_t *read_p
     }
 }
 
+// base64 functions copied from: https://stackoverflow.com/a/6782480/8044326
+static void build_decoding_table() {
+
+    decoding_table = malloc(256);
+
+    for (int i = 0; i < 64; i++)
+        decoding_table[(unsigned char)encoding_table[i]] = i;
+}
+
+
+static void base64_cleanup() {
+    free(decoding_table);
+}
+
+
+static unsigned char* base64_decode(const char* data, size_t input_length, size_t* output_length) {
+
+    if (decoding_table == NULL) build_decoding_table();
+
+    if (input_length % 4 != 0) return NULL;
+
+    *output_length = input_length / 4 * 3;
+    if (data[input_length - 1] == '=') (*output_length)--;
+    if (data[input_length - 2] == '=') (*output_length)--;
+
+    unsigned char* decoded_data = malloc(*output_length);
+    if (decoded_data == NULL) return NULL;
+
+    for (int i = 0, j = 0; i < input_length;) {
+
+        uint32_t sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+        uint32_t sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+
+        uint32_t triple = (sextet_a << 3 * 6)
+            + (sextet_b << 2 * 6)
+            + (sextet_c << 1 * 6)
+            + (sextet_d << 0 * 6);
+
+        if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+    }
+
+    return decoded_data;
+}
+
+
 static int generate_YUV4MPEG(parameters_t *param)
 {
   ssize_t jpegsize;
@@ -481,6 +544,18 @@ static int generate_YUV4MPEG(parameters_t *param)
   y4m_frame_info_t frameinfo;
   jpegsize = 0;
   loops = param->loop;
+
+  char* test = "VGVzdCBpbnB1dCBlbmNvZGUgZm9yIGJhc2U2NA==";
+  size_t input_len = 40;
+  size_t output_len = 0;
+
+  uint8_t* buffer = base64_decode(test, input_len, &output_len);
+  mjpeg_info("Output length: %d", output_len);
+  for (int i = 0; i < output_len; i++) {
+      mjpeg_info("%c", buffer[i]);
+  }
+  base64_cleanup();
+
 
   while (1) {
 
